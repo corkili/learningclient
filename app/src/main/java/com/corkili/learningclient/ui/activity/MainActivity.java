ddba87ee0a49cfb22046505705963e5bc498a521
corkili <corkili@163.com>
2019-03-08 13:44:34 +0800

    #1 创建项目

---
+++ app/src/main/java/com/corkili/learningclient/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.corkili.learningclient.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.corkili.learningclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_activity);
+    }
+}
