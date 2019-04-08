2ef0e7d4af707827fd6d4ff8d35f078c62ad87bf
corkili <corkili@163.com>
2019-04-08 22:17:56 +0800

    #6 登录保存账户密码

--- app/src/main/java/com/corkili/learningclient/ui/activity/LoginActivity.java
+++ app/src/main/java/com/corkili/learningclient/ui/activity/LoginActivity.java
@@ -2,15 +2,20 @@ package com.corkili.learningclient.ui.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import com.corkili.learningclient.R;
 import com.corkili.learningclient.common.IntentParam;
+import com.corkili.learningclient.common.SPParam;
 import com.corkili.learningclient.common.UIHelper;
 import com.corkili.learningclient.generate.protobuf.Info.UserInfo;
 import com.corkili.learningclient.generate.protobuf.Info.UserType;
@@ -19,6 +24,8 @@ import com.corkili.learningclient.service.ServiceResult;
 import com.corkili.learningclient.service.UserService;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -49,6 +56,22 @@ public class LoginActivity extends AppCompatActivity {
         userTypeRadioGroup = findViewById(R.id.radio_group_user_type);
         loginButton = findViewById(R.id.button_login);
         toRegisterButton = findViewById(R.id.button_to_register);
+
+        SharedPreferences sp = getSharedPreferences(SPParam.SP_NAME_USER_INFO, MODE_PRIVATE);
+        String phone = sp.getString(SPParam.SP_KEY_PHONE, "");
+        String userType = sp.getString(SPParam.SP_KEY_USER_TYPE, "");
+
+        String password;
+        if (SPParam.SP_VALUE_USER_TYPE_STUDENT.equals(userType)) {
+            userTypeRadioGroup.check(R.id.user_type_student);
+            password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_STUDENT, "");
+        } else {
+            userTypeRadioGroup.check(R.id.user_type_teacher);
+            password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_TEACHER, "");
+        }
+        phoneEditText.setText(phone);
+        passwordEditText.setText(password);
+
         Intent intent = getIntent();
         if (intent != null) {
             UserInfo userInfo = (UserInfo) intent.getSerializableExtra(IntentParam.USER_INFO);
@@ -59,6 +82,7 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     userTypeRadioGroup.check(R.id.user_type_student);
                 }
+                passwordEditText.setText("");
             }
         }
         initListener();
@@ -87,18 +111,75 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
             LoginActivity.this.finish();
         });
+
+        phoneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String phoneNow = phoneEditText.getText().toString().trim();
+                SharedPreferences sp = getSharedPreferences(SPParam.SP_NAME_USER_INFO, MODE_PRIVATE);
+                String phone = sp.getString(SPParam.SP_KEY_PHONE, "");
+                if (StringUtils.isBlank(phone)) {
+                    return;
+                }
+                if (!phone.equals(phoneNow)) {
+                    passwordEditText.setText("");
+                } else {
+                    String password;
+                    if (userTypeRadioGroup.getCheckedRadioButtonId() == R.id.user_type_student) {
+                        password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_STUDENT, "");
+                    } else {
+                        password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_TEACHER, "");
+                    }
+                    passwordEditText.setText(password);
+                }
+            }
+        });
+
+        userTypeRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            String phoneNow = phoneEditText.getText().toString().trim();
+            SharedPreferences sp = getSharedPreferences(SPParam.SP_NAME_USER_INFO, MODE_PRIVATE);
+            String phone = sp.getString(SPParam.SP_KEY_PHONE, "");
+            if (phone != null && !phone.equals(phoneNow)) {
+                return;
+            }
+            String password = "";
+            if (checkedId == R.id.user_type_student) {
+                password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_STUDENT, "");
+            } else if (checkedId == R.id.user_type_teacher) {
+                password = sp.getString(SPParam.SP_KEY_PASSWORD_FOR_TEACHER, "");
+            }
+            passwordEditText.setText(password);
+        });
     }
 
     private void handleLoginMsg(Message msg) {
         ServiceResult serviceResult = (ServiceResult) msg.obj;
         if (serviceResult.isSuccess()) {
             UserInfo userInfo = serviceResult.extra(UserLoginResponse.class).getUserInfo();
+            SharedPreferences sp = getSharedPreferences(SPParam.SP_NAME_USER_INFO, MODE_PRIVATE);
+            Editor editor = sp.edit();
+            editor.putString(SPParam.SP_KEY_PHONE, phoneEditText.getText().toString().trim());
             Intent intent = new Intent();
             if (userInfo.getUserType() == UserType.Teacher) {
+                editor.putString(SPParam.SP_KEY_PASSWORD_FOR_TEACHER, passwordEditText.getText().toString().trim());
+                editor.putString(SPParam.SP_KEY_USER_TYPE, SPParam.SP_VALUE_USER_TYPE_TEACHER);
                 intent.setClass(LoginActivity.this, TeacherMainActivity.class);
             } else {
+                editor.putString(SPParam.SP_KEY_PASSWORD_FOR_STUDENT, passwordEditText.getText().toString().trim());
+                editor.putString(SPParam.SP_KEY_USER_TYPE, SPParam.SP_VALUE_USER_TYPE_STUDENT);
                 intent.setClass(LoginActivity.this, StudentMainActivity.class);
             }
+            editor.apply();
             intent.putExtra(IntentParam.USER_INFO, userInfo);
             startActivity(intent);
             LoginActivity.this.finish();
